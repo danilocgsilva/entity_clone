74e3ab620edb26b7e1a7cec1b897f4fd2f60bd3c
Danilo Silva <contact@danilocgsilva.me>
2023-01-12 20:03:13 +0000

    Changed methods for main class

--- src/EntityClone.php
+++ src/EntityClone.php
@@ -4,27 +4,48 @@ declare(strict_types=1);
 
 namespace Danilocgsilva\EntityClone;
 
-use AddressData;
-
 class EntityClone {
     
-    private AddressData $source;
-    private AddressData $destiny;
+    private string $sourceDns;
+    private string $sourceUser;
+    private string $sourcePassword;
+    private string $destinyDns;
+    private string $destinyUser;
+    private string $destinyPassword;
     
-    public function setSource(AddressData $source): self
+    public function setSourceDns(string $sourceDns): self
     {
-        $this->source = $source;
+        $this->sourceDns = $sourceDns;
         return $this;
     }
 
-    public function setDestiny(AddressData $destiny): self
+    public function setSourceUser(string $soureUser): self
     {
-        $this->destiny = $destiny;
+        $this->sourceUser = $soureUser;
         return $this;
     }
 
-    public function discoverEntity(string $entity): void
+    public function setSourcePassword(string $sourcePassword): self
     {
-        
+        $this->sourcePassword = $sourcePassword;
+        return $this;
+    }
+
+    public function setDestinyDns(string $destinyDns): self
+    {
+        $this->destinyDns = $destinyDns;
+        return $this;
+    }
+
+    public function setDestinyUser(string $destinyUser): self
+    {
+        $this->destinyUser = $destinyUser;
+        return $this;
+    }
+
+    public function setDestinyPassword(string $destinyPassword): self
+    {
+        $this->destinyPassword = $destinyPassword;
+        return $this;
     }
 }
